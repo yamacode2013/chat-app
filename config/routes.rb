8b212b2e0693992956df155e26f8460e9a34fc00
yamacode2013 <142188024+yamacode2013@users.noreply.github.com>
2023-08-26 10:18:52 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ↑↑上の記述は削除しましょう↑↑
-  root to: "messages#index"
+  devise_for :users
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
